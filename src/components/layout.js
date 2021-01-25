fb86ce606d23d66c568f99089bbb4ed3176adbbd
Kota Kanazawa <kota0818@gmail.com>
2021-01-25 21:04:57 +0900

    Add Header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "../styles/layout.css"
+import Header from './header'
 
 const Layout = ({ children }) => {
-    return (
-        <div className="layout">
-            <main>{children}</main>
-        </div>
-    )
+  return (
+    <div className="layout">
+      <Header />
+      <main>{children}</main>
+    </div>
+  )
 }
 
 export default Layout
