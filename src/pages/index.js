8d224f4a593b2cd721971983768462c72bb36455
Kota Kanazawa <kota0818@gmail.com>
2021-02-07 12:24:50 +0900

    MicroCMSから記事情報を取得して一覧する

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,36 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import PostLink from "../components/post-link"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
-      <PostLink />
+      <ul>
+        {data.allMicrocmsBlog.edges.map(({ node }) => (
+          <li key={node.blogId}>
+            <Link to={`/blog/${node.blogId}`}>{node.title}</Link>
+          </li>
+        ))}
+      </ul>
       <PostLink />
       <PostLink />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMicrocmsBlog {
+      edges {
+        node {
+          blogId
+          title
+          body
+          slug
+          publishedAt
+        }
+      }
+    }
+  }
+`
