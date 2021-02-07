832ea286b2ed1f42eef811d31481c7ba078b2324
Kota Kanazawa <kota0818@gmail.com>
2021-02-07 15:59:52 +0900

    Remove unused component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import PostLink from "../components/post-link"
 
 export default function Home({ data }) {
   return (
