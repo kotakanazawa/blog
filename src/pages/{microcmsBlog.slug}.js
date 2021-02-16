2c0ed100efa18a290153b2d9d3d0163f9c09114f
Kota Kanazawa <kota0818@gmail.com>
2021-02-16 21:25:01 +0900

    ()ではなく{}で返す

--- src/pages/{microcmsBlog.slug}.js
+++ src/pages/{microcmsBlog.slug}.js
@@ -4,23 +4,25 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BIO from "../components/bio"
 
-const BlogPage = ({ data, location }) => (
-  <Layout>
-    <SEO
-      title={data.microcmsBlog.title}
-      description={data.microcmsBlog.description}
-      pagepath={location.pathname}
-    />
-    <h1>{data.microcmsBlog.title}</h1>
-    <span>{data.microcmsBlog.publishedAt}</span>
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `${data.microcmsBlog.body}`,
-      }}
-    />
-    <BIO />
-  </Layout>
-)
+const BlogPage = ({ data, location }) => {
+  return (
+    <Layout>
+      <SEO
+        title={data.microcmsBlog.title}
+        description={data.microcmsBlog.description}
+        pagepath={location.pathname}
+      />
+      <h1>{data.microcmsBlog.title}</h1>
+      <span>{data.microcmsBlog.publishedAt}</span>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${data.microcmsBlog.body}`,
+        }}
+      />
+      <BIO />
+    </Layout>
+  )
+}
 
 export default BlogPage
 
