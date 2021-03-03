d6dc31c4b3aa9dd30ea89263a5e685779ad817bd
Kota Kanazawa <kota0818@gmail.com>
2021-03-03 21:28:13 +0900

    フッタータイトルにトップページへのリンク追加

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 import "../styles/footer.css"
 
 export default function Footer() {
   return (
     <footer className="footer">
-      © {new Date().getFullYear()} Just Do IT
+      © {new Date().getFullYear()} <Link to={`/`}>Just Do IT</Link>
     </footer>
   )
 }
