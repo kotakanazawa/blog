859bf6aa554085c806ea6bd810e8ed513bab1711
Kota Kanazawa <kota0818@gmail.com>
2021-07-04 12:47:19 +0900

    topページ作成

--- pages/404.js
+++ pages/404.js
@@ -5,3 +5,18 @@ export default function Custom404() {
     </main>
   )
 }
+
+// import Layout from "../components/layout"
+// import SEO from "../components/seo"
+
+// export default ({ location }) => {
+//   return (
+//     <Layout>
+//       <SEO
+//         title="ページが見つかりません(404エラー)"
+//         pagepath={location.pathname}
+//       />
+//       <h1 style={{ padding: `20vh 0`, textAlign: `center` }}>お探しのページが見つかりませんでした。ごめんなさい。</h1>
+//     </Layout>
+//   )
+// }
