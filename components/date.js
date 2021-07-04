b77e92fe8ef44af02d5fd38ebb7c8d3e4f4da717
Kota Kanazawa <kota0818@gmail.com>
2021-07-04 20:38:16 +0900

    記事公開の日付を修正

--- components/date.js
+++ components/date.js
@@ -3,5 +3,5 @@ import ja from 'date-fns/locale/ja'
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'yyyy年MM月dd日(iii)', {locale: ja})}</time>
+  return <time dateTime={dateString}>{format(date, 'yyyy/MM/dd', { locale: ja })}</time>
 }
