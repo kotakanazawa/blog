7ac9d9b4349ab645514b8864aa3b80dcb77459c1
Kota Kanazawa <kota0818@gmail.com>
2021-07-23 09:20:49 +0900

    Use TS in date.js

--- components/date.tsx
+++ components/date.tsx
@@ -1,7 +1,7 @@
 import { parseISO, format } from 'date-fns'
 import ja from 'date-fns/locale/ja'
 
-export default function Date({ dateString }) {
+export default function Date({ dateString }: { dateString: string}) {
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'yyyy/MM/dd', { locale: ja })}</time>
 }
