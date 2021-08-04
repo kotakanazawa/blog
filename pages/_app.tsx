2132c5e7b26bc4754be32e1ca2f6176f513c490f
Kota Kanazawa <kota0818@gmail.com>
2021-08-04 21:18:21 +0900

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { GA_TRACKING_ID, pageview } from '../libs/gtag';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    if (!GA_TRACKING_ID) return
+
+    const handleRouteChange = (url: string) => {
+      pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
